1d7325364acd47dfc7e3451e3d56c2842a507a7b
sabrynaElkind <sabryna.elkind11@gmail.com>
2023-06-21 12:25:01 +0200

    fixed6

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+
+        if (third<second && second<first ||second<third && third<first ){
+            System.out.println(first);
+        } else if (first<second && first< third || second <first && first<third){
+            System.out.println(third);
+        } else {
+            System.out.println(second);
+        }
     }
 }
